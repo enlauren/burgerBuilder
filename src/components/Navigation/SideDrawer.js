82e8f16b00f84762568f7f309e812edfe773c96e
Laurentiu Enache <enlau101@gmail.com>
2018-07-30 09:49:37 +0100

    https://reactburger-dd619.firebaseapp.com/

--- src/components/Navigation/SideDrawer.js
+++ src/components/Navigation/SideDrawer.js
@@ -1,30 +1,32 @@
 import React from "react";
-import Logo from '../../components/Logo'
+import Logo from "../../components/Logo";
 import NavigationItems from "./NavigationItems";
-import classes from './SideDrawer.css'
-import BackDrop from '../BackDrop'
+import classes from "./SideDrawer.css";
+import BackDrop from "../BackDrop";
 import Auxi from "../../hoc/Auxi";
 
-const SideDrawer = (props) => {
-
+const SideDrawer = props => {
     let attachedClasses = [classes.SideDrawer, classes.Close];
-    if(props.open)  {
-        attachedClasses=[classes.SideDrawer, classes.Open]
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
     }
 
-  return (
-      <Auxi>
-      <BackDrop show={props.open} clicked={props.closed} />
-      <div className={attachedClasses.join(" ")}>
-          <div className={classes.Logo}>
-          <Logo />
-          </div>
-          <nav>
-              <NavigationItems token={props.token} isAuth={props.isAuth}/>  
-          </nav>
-      </div>
-      </Auxi>
-  );
-}
+    return (
+        <Auxi>
+            <BackDrop show={props.open} clicked={props.closed} />
+            <div className={attachedClasses.join(" ")} onClick={props.closed}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems
+                        token={props.token}
+                        isAuth={props.isAuth}
+                    />
+                </nav>
+            </div>
+        </Auxi>
+    );
+};
 
 export default SideDrawer;
